779518e9b28b34cc769ea4a5ceae7134a361a491
Joy Ayitey <jijakahn6@gmail.com>
2025-09-13 09:04:36 +0000

    feat: implement get all reports service
    - Used reportType for setting report type in url
    - Filtered report type based on enum and Url value
    - Returned filtered result in stringified JSON

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { data, ReportType } from 'src/data';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllIncomeReport(reportType: string): string {
+		const resolvedType =
+			reportType === 'income'  ? ReportType.INCOME : ReportType.EXPENSE;
+		const filteredReport =
+			data.report.filter((report) => report.reportType === resolvedType);
+		return JSON.stringify(filteredReport);
   }
+
+	getIncomeReportById(): string {
+		return 'Report for ID...';;
+	}
+
+	createIncomeReport(): string {
+		return 'Creating new data ...';;
+	}
+
+	updateIncomeReport(): string {
+		return 'Updating data for ID...';;
+	}
+
+	deleteIncomeReportById(): string {
+		return 'Deleting data for ID...';;
+	}
 }
