c25dfc25fb890d62b77d5b14a64a9d6050616923
Joy Ayitey <jijakahn6@gmail.com>
2025-09-13 09:03:05 +0000

    feat: Implemented Get All Reports controllers
    - Added report/ for generic controller default path
    - Added url for getting all reports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('report/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':reportType')
+  getAllIncomeReport(
+		@Param('reportType') reportType: 'string'
+	): string {
+    return this.appService.getAllIncomeReport(reportType);
   }
+
+	@Get(':id')
+	getIncomeReportById(): string {
+		return this.appService.getIncomeReportById();
+	};
+
+	@Post()
+	createIncomeReport(): string {
+		return this.appService.createIncomeReport();
+	};
+
+	@Put(':id')
+	upateIncomeReport(): string {
+		return this.appService.updateIncomeReport();
+	};
+
+	@Delete(':id')
+	deleteIncomeReportById(): string {
+		return this.appService.deleteIncomeReportById();
+	};
+
 }
